921ed4d162ce4d43f2b46643d18107a5c3198322
USUARIO <lourdes.99101@gmail.com>
2023-11-30 22:48:00 -0500

    "Luz"

--- app/src/main/java/com/example/agregar_editar/adapter/HamburguesaAdapter.kt
+++ app/src/main/java/com/example/agregar_editar/adapter/HamburguesaAdapter.kt
@@ -3,6 +3,8 @@ package com.example.agregar_editar.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -10,25 +12,27 @@ import com.example.agregar_editar.R
 import com.example.agregar_editar.entity.Hamburguesa_list
 import com.squareup.picasso.Picasso
 
-class HamburguesaAdapter (private val dataset: MutableList<Hamburguesa_list>):
-    RecyclerView.Adapter<HamburguesaAdapter.ViewHolder>() {
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-          val txtDescripcion : TextView
-        val imagen : ImageView
-        val txtNombre : TextView
+class HamburguesaAdapter(private var dataset: MutableList<Hamburguesa_list>) :
+    RecyclerView.Adapter<HamburguesaAdapter.ViewHolder>(), Filterable {
+    public var listaFiltrada: List<Hamburguesa_list> = dataset
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val txtDescripcion: TextView
+        val imagen: ImageView
+        val txtNombre: TextView
+
         init {
-            txtNombre =itemView.findViewById(R.id.txt_nombre)
+            txtNombre = itemView.findViewById(R.id.txt_nombre)
             txtDescripcion = itemView.findViewById(R.id.txt_descripcion)
             imagen = itemView.findViewById(R.id.imagen)
-       }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from (parent.context).inflate(R.layout.listado_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.listado_item, parent, false)
         return HamburguesaAdapter.ViewHolder(view)
     }
 
-    override fun getItemCount()= dataset.size
+    override fun getItemCount() = dataset.size
 
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -37,5 +41,37 @@ class HamburguesaAdapter (private val dataset: MutableList<Hamburguesa_list>):
         holder.txtNombre.text = receta.nombre
         holder.txtDescripcion.text = receta.descripcion
         Picasso.get().load(receta.imagen).into(holder.imagen)
+    }
+    // Método para realizar la filtración
+    override fun getFilter(): Filter {
+        var total_burguer: List<Hamburguesa_list> = dataset
+        return object : Filter() {
+            override fun performFiltering(charSequence: CharSequence): FilterResults {
+                val filtrarResultados = FilterResults()
+                val filtrados = mutableListOf<Hamburguesa_list>()
+
+                if (charSequence.isEmpty()) {
+                    filtrados.addAll(total_burguer)
+                } else {
+                    val filtro = charSequence.toString().toLowerCase().trim()
+                    for (hamburguesa in total_burguer) {
+                        if (hamburguesa.nombre.toLowerCase().contains(filtro)) {
+                            filtrados.add(hamburguesa)
+                        }
+                    }
+                }
+
+                filtrarResultados.values = filtrados
+                return filtrarResultados
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            override fun publishResults(charSequence: CharSequence?, results: FilterResults?) {
+                dataset = results?.values as? MutableList<Hamburguesa_list> ?: mutableListOf()
+                notifyDataSetChanged()
+            }
+
+
+        }
     }
 }
--- app/src/main/java/com/example/agregar_editar/vistas/ListadoActivity.kt
+++ app/src/main/java/com/example/agregar_editar/vistas/ListadoActivity.kt
@@ -1,14 +1,20 @@
 package com.example.agregar_editar.vistas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.get
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.agregar_editar.MainActivity
 import com.example.agregar_editar.R
 import com.example.agregar_editar.adapter.HamburguesaAdapter
 import com.example.agregar_editar.entity.Hamburguesa_list
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -17,13 +23,14 @@ class ListadoActivity : AppCompatActivity() {
     lateinit var adapter: HamburguesaAdapter
     lateinit var recyclerView: RecyclerView
     lateinit var txtEncontrados : TextView
-
+    lateinit var txtBuscar : TextInputLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_listado)
-        txtEncontrados = findViewById(R.id.txt_resultado)
-        recyclerView = findViewById(R.id.recycler_lista)
-        val db = Firebase.firestore
+        setContentView(R.layout.search_view)
+        txtEncontrados = findViewById(R.id.txt_resultados)
+        recyclerView = findViewById(R.id.recycler_list)
+        txtBuscar = findViewById(R.id.txt_buscar)
+        /*val db = Firebase.firestore
 
         val lista = mutableListOf<Hamburguesa_list>()
         db.collection("Hamburguesa_lista").get()
@@ -38,6 +45,44 @@ class ListadoActivity : AppCompatActivity() {
             }.addOnFailureListener{
                 Toast.makeText(baseContext,"Error al cargar", Toast.LENGTH_SHORT).show()
             }
+        recyclerView.layoutManager = LinearLayoutManager(this)*/
+    }
+    fun listar(terminoBusqueda: String? = null) {
+        val db = Firebase.firestore
+        val lista = mutableListOf<Hamburguesa_list>()
+
+        db.collection("Hamburguesa_lista").get()
+            .addOnSuccessListener { resultado ->
+                for (documento in resultado) {
+                    lista.add(documento.toObject(Hamburguesa_list::class.java))
+                }
+
+                val adapter = HamburguesaAdapter(lista)
+                recyclerView.adapter = adapter
+                adapter.filter.filter(terminoBusqueda)
+
+                val cantidad = adapter.listaFiltrada.size
+
+                txtEncontrados.text = " $cantidad Hamburguesas encontradas"
+            }
+            .addOnFailureListener {
+                Toast.makeText(baseContext, "Error al cargar", Toast.LENGTH_SHORT).show()
+            }
+
         recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+
+    fun buscarHamburguesa(view: View) {
+        val terminoBusqueda = txtBuscar.editText?.text?.toString()
+        Toast.makeText(this, terminoBusqueda, Toast.LENGTH_SHORT).show()
+
+        listar(terminoBusqueda)
+    }
+
+
+
+    fun editar(view: View){
+        startActivity(Intent(this, MainActivity::class.java))
     }
 }
